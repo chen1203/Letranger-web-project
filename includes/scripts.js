b4e42d5b547ccdc93e3c587ed0473503dfb395ff
tom243 <tomer.luster@gmail.com>
2015-01-04 01:32:49 +0200

    add jquery
    try fix footer

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-  var bodyHeight = $("body").height();
-  var vwptHeight = $(window).height();
-  if (vwptHeight > bodyHeight) {
-    $("footer").css("position","absolute").css("bottom",0);
-  }
-});
+	$(document).ready(function(){
+  		$(window).resize(function(){
+			    var footerHeight = $('footer').outerHeight();
+			    var stickFooterPush = $('.push').height(footerHeight);
+		
+    			$('#wrapper').css({'marginBottom':'-' + footerHeight-282 + 'px'});
+		    });
+		
+    		$(window).resize();
+	    });
