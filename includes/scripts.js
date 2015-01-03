d1fed94c7595264bd30a9671af7593dc340927a7
tom243 <tomer.luster@gmail.com>
2015-01-04 01:18:50 +0200

    STAM

---
+++ includes/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var bodyHeight = $("body").height();
+  var vwptHeight = $(window).height();
+  if (vwptHeight > bodyHeight) {
+    $("footer").css("position","absolute").css("bottom",0);
+  }
+});
