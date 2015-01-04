204d6dd8b8cae7821fd77b55d673cc1b7a53bc10
tom243 <tomer.luster@gmail.com>
2015-01-04 12:28:39 +0200

    FOOTER WORK!!!!!!!!!!!

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,10 +1,21 @@
-	$(document).ready(function(){
+	/*$(document).ready(function(){
   		$(window).resize(function(){
 			    var footerHeight = $('footer').outerHeight();
 			    var stickFooterPush = $('.push').height(footerHeight);
-		
-    			$('#wrapper').css({'marginBottom':'-' + footerHeight-282 + 'px'});
+				var a=282;
+    			$('#wrapper').css({'marginBottom':'-' + footerHeight + 'px'});
 		    });
 		
     		$(window).resize();
-	    });
+	    });*/
+	   
+  $(document).ready(function() {
+   
+   var docHeight = $(window).height();
+   var footerHeight = $('footer').height();
+   var footerTop = $('footer').position().top + footerHeight;
+   
+   if (footerTop < docHeight) {
+    $('footer').css('margin-top',(docHeight - footerTop) + 'px');
+   }
+  });
