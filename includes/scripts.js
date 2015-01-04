f1600be99abfe0933e91ca8beeb3c91d3c9a1b44
tom243 <tomer.luster@gmail.com>
2015-01-04 15:35:29 +0200

    SADSAD

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/*	$(document).ready(function(){
-  		$(window).resize(function(){
-			    var footerHeight = $('footer').outerHeight();
-			    var stickFooterPush = $('.push').height(footerHeight);
-		
-    			$('#wrapper').css({'marginBottom':'-' + footerHeight-282 + 'px'});
-		    });
-		
-    		$(window).resize();
-	    });
-
-
-*/
-/*
-$(document).ready(function() {
-  var bodyHeight = $("body").height();
-  var vwptHeight = $(window).height();
-  if (vwptHeight > bodyHeight) {
-    $("footer#colophon").css("position","absolute").css("bottom",0);
-  }
-});
-*/
-=======
 $(document).ready(function() {
    var docHeight = $(window).height();
    var footerHeight = $('footer').height();
@@ -31,4 +7,4 @@ $(document).ready(function() {
     	$('footer').css('margin-top',(docHeight - footerTop) + 'px');
    }
 });
->>>>>>> origin/master
+
