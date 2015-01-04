cd7f47f2509f5d24244ea3202229c4e45ef7add1
Chen Shamir <chenshamir1203@gmail.com>
2015-01-04 19:11:48 +0200

    final

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,10 +1,13 @@
+/*
+ *  function to set the footer to bottom of the page while considering the screen size
+ */
+
 $(document).ready(function() {
-   var docHeight = $(window).height();
-   var footerHeight = $('footer').height();
-   var footerTop = $('footer').position().top + footerHeight;
-   
-   if (footerTop < docHeight) {
-    	$('footer').css('margin-top',(docHeight - footerTop) + 'px');
+	   var docHeight = $(window).height();
+	   var footerHeight = $('footer').height();
+	   var footerTop = $('footer').position().top + footerHeight;
+	   if (footerTop < docHeight) {
+	    		$('footer').css('margin-top',(docHeight - footerTop) + 'px');
    }
 });
 
