6aa5978c16318c44639520477da8f2b94c09da50
tom243 <tomer.luster@gmail.com>
2015-01-19 10:44:41 +0200

    articles

--- includes/scripts.js
+++ includes/scripts.js
@@ -3,11 +3,14 @@
  */
 
 $(document).ready(function() {
-	   var docHeight = $(window).height();
-	   var footerHeight = $('footer').height();
-	   var footerTop = $('footer').position().top + footerHeight;
-	   if (footerTop < docHeight) {
-	    		$('footer').css('margin-top',(docHeight - footerTop) + 'px');
+		//var currentId = $('body').attr('id');
+	  // if (currentId != "articles"){	
+			var docHeight = $(window).height();
+			var footerHeight = $('footer').height();
+			var footerTop = $('footer').position().top + footerHeight;
+			if (footerTop < docHeight) {
+			    		$('footer').css('margin-top',(docHeight - footerTop) + 'px');
+	   		//}
    }
 });
 
