ab27d2309f19f14d0affbb0766ff7564b31f6340
tom243 <tomer.luster@gmail.com>
2015-01-20 15:42:30 +0200

    check scrolling

--- includes/scripts.js
+++ includes/scripts.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
 
 var BackgroundScroll = function(params) {
 	params = $.extend({
-		scrollSpeed: 70,
-		imageWidth: $('#textual_pages').width(),
-		imageHeight: $('#textual_pages').height()
+		scrollSpeed: 20,
+		imageWidth: $('#hp').width(),
+		imageHeight: $('#hp').height()
 	}, params);
 	
 	var step = 1,
@@ -30,7 +30,7 @@ var BackgroundScroll = function(params) {
 		if (current == restartPosition){
 			current = 0;
 		}	
-		$('#textual_pages').css('backgroundPosition', current + 'px 0');
+		$('#hp').css('backgroundPosition', current + 'px 0');
 	
 	};
 	
@@ -42,3 +42,22 @@ var BackgroundScroll = function(params) {
 
 var scroll = new BackgroundScroll();
 scroll.init();
+
+
+/*$(document).ready(function(){
+    var vH=310;
+    var vW=2211;
+    var vT=$('#textual_pages').offset().top;
+    var vL=$('#textual_pages').offset().left;
+    var vR=$('#textual_pages').offset().right;
+    $('#textual_pages').mousemove(function(e){
+    	var xposright=e.pageX-vR;
+        var ypos=e.pageY-vT;
+        var xpos=e.pageX-vL;
+        var y=Math.round(ypos/vW*100);
+        var x=Math.round(xpos/vH*100);
+        var xright=Math.round(xposright/vH*100);
+        $('#textual_pages').css({backgroundPositionX: x+'%'});
+        $('#textual_pages').css({backgroundPositionX: xright+'%'});
+    });
+});*/
