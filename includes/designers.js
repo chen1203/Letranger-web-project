f5775b173190df606094bfab5650943e74a7cf8f
tom243 <tomer.luster@gmail.com>
2015-02-20 21:48:54 +0200

    Dynamics WORKING!

--- includes/designers.js
+++ includes/designers.js
@@ -1,22 +1,23 @@
-var designerDescription, designerName;
 function getDesignerName() {
 	var aKeyValue = window.location.search.substring(1).split("&");
 	var designerName = aKeyValue[0].split("=")[1];
-	
+	designerName = designerName.replace(/%[0-9]+/g," ");
 	console.log(designerName);
-	//console.log("in function getDesignerName-> designerName: "+designerName);
 	return designerName;
 };
 
 $.getJSON("includes/designers.json",function(data){
+	var designerDescription, designerName, designerPics;
 	var name = getDesignerName();//get data by url params
 	$.each(data.designers, function(key, value) {
-		console.log("designerName: "+name);
 	    if (value.name == name) {
 	        designerName = value.name;
             designerDescription = value.description;
+            designerPics=value.pics[0];
+            console.log("designerPics: "+designerPics);
 	    }
 	});
-	$("#designer_name_title").html(designerName);
+	$("#designer_name_title").prepend(designerName +"");
 	$("#designer_desc>p").html(designerDescription);
+	$("#designer_pics").append("<img src='images/designers/"+designerName+"/"+designerPics+".jpg'>");
 });
