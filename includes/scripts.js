febacd2584a5764e00dce1b0ec02e85c2003798b
tom243 <tomer.luster@gmail.com>
2015-02-22 21:06:22 +0200

    change logo position to % and change arrows to links

--- includes/scripts.js
+++ includes/scripts.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
 
 /*Background scrolling pictures start*/
 
+/*
 var BackgroundScroll = function(params) {
 	params = $.extend({
 		scrollSpeed: 20,
@@ -76,7 +77,8 @@ var BackgroundScroll = function(params) {
 };
 
 var scroll = new BackgroundScroll();
-scroll.init();
+scroll.init();*/
+
 
 /*Background scrolling end*/
 
