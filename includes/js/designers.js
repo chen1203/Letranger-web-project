e14589323084bc539ef5aa5ceb7a723811635166
Chen Shamir <chenshamir1203@gmail.com>
2015-02-23 21:12:35 +0200

    almost end...

--- includes/js/designers.js
+++ includes/js/designers.js
@@ -1,60 +1,52 @@
 $(document).ready(function () { 
+	
+	function getDesignerName() {
+		var aKeyValue = window.location.search.substring(1).split("&");
+		var designer = aKeyValue[0].split("=")[1];
+		designerName = designer.replace(/%[0-9]+/g," ");
+		desNameWithSep = designer.replace(/%[0-9]+/g,"_");
+		return designerName;
+	};
 
-function getDesignerName() {
-	var aKeyValue = window.location.search.substring(1).split("&");
-	var designer = aKeyValue[0].split("=")[1];
-	designerName = designer.replace(/%[0-9]+/g," ");
-	desNameWithSep = designer.replace(/%[0-9]+/g,"_");
-	return designerName;
-};
-
- var options = {
+ 	var options = {
                 $AutoPlay: true,
-
-                $PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slideshow is auto playing, default value is false
-
-                $ArrowKeyNavigation: true,   			            //Allows arrow key to navigate or not
+                $PauseOnHover: 1,                               	//[Optional] Whether to pause when mouse over if a slideshow is auto playing, default value is false
+                $ArrowKeyNavigation: true,   			       //Allows arrow key to navigate or not
                 $SlideWidth: 334,                                   //[Optional] Width of every slide in pixels, the default is width of 'slides' container
                 $SlideHeight: 500,                                  //[Optional] Height of every slide in pixels, the default is width of 'slides' container
                 $SlideSpacing: 2, 					                //Space between each slide in pixels
                 $DisplayPieces: 2,                                  //Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
                 $ParkingPosition: 166,                                //The offset position to park slide (this options applys only when slideshow disabled).
-
                 $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
                     $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
                     $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
                     $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
                     $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
                 }
-            };
+     };
 
-$.getJSON("includes/designers.json",function(data){
-	var designerDescription, designerName, designerPics;
-	var name = getDesignerName();//get data by url params
-	
+	$.getJSON("includes/designers.json", function(data) {
+		var designerDescription, designerName, designerPics;
+		var name = getDesignerName();	//get data by url params
 
-	$.each(data.designers, function(key, value) {
-	    if (value.name == name) {
-	        designerName = value.name;
-	        console.log(designerName);
-	        
-            designerDescription = value.description;
-            designerPics=value.pics;
-            console.log("designerPics: "+designerPics);
-            
-            $('#'+desNameWithSep+'').addClass("selected_designer");
-	    }
-	});
-	 document.title = designerName; // Set the title to designer name
-	$("#designer_name_title").prepend(designerName +" &nbsp;\\&nbsp; mens suits");
-	$("#designer_desc>p").html(designerDescription);
-		$.each( designerPics, function( i, pic ) {
-			picI = "images/designers/"+designerName+"/"+pic+".jpg";
-			$('#designer_pics').append("<div> <img u=image src='"+picI+"'> </div> ");
+		$.each(data.designers, function(key, value) {
+			if (value.name == name) {
+				designerName = value.name;
+				designerDescription = value.description;
+				designerPics = value.pics;
+				$('#' + desNameWithSep + '').addClass("selected_designer");
+			}
+		});
+		document.title = designerName;
+		// Set the title to designer name
+		$("#designer_name_title").prepend(designerName + " &nbsp;\\&nbsp; mens suits");
+		$("#designer_desc>p").html(designerDescription);
+		$.each(designerPics, function(i, pic) {
+			picI = "images/designers/" + designerName + "/" + pic + ".jpg";
+			$('#designer_pics').append("<div> <img u=image src='" + picI + "'> </div> ");
 		});
 		var jssor_slider1 = new $JssorSlider$("slider1_container", options);
 	});
-	
-	
+
 });
 
---
+++ includes/js/scripts.js
@@ -0,0 +1,125 @@
+/*----------------------- Fixed footer  ----------------------- */
+
+/*
+function positionFooter() { 
+	var mFoo = $("footer"); 
+	if ((($(document.body).height() + mFoo.outerHeight()) < $(window).height() && mFoo.css("position") == "fixed") 
+		|| ($(document.body).height() < $(window).height() && mFoo.css("position") != "fixed")) { 
+					mFoo.css({ position: "fixed", bottom: "0px" }); 
+			} 
+	else { mFoo.css({ position: "fixed" }); 
+	} 
+} 
+$(document).ready(function () { 
+	positionFooter();
+	$(window).scroll(positionFooter); 
+	$(window).resize(positionFooter); 
+	$(window).load(positionFooter); 
+});*/
+	
+/*----------------------- Open footer  ----------------------- */
+
+$(document).ready(function(){
+	
+	$(".btn-slide").click(function(){
+			
+		$("#openFooter").slideToggle("slow");
+		if ($("#login_logout-slide").hasClass("closedFooter")) {
+			$("#login_logout-slide").css("background-image","url(images/down_arrow.png)");
+			$("#online_shop-slide").css("background-image","url(images/down_arrow.png)");
+			$("#login_logout-slide").removeClass("closedFooter");
+		} else {
+			$("#login_logout-slide").css("background-image","url(images/footer_up_arrow.png)");
+			$("#online_shop-slide").css("background-image","url(images/footer_up_arrow.png)");
+			$("#login_logout-slide").addClass("closedFooter");
+		}
+		return false;
+		
+	});
+});
+
+/*----------------------- Hamburger change menu ----------------------- */
+
+$(document).ready(function(){
+	
+	$(window).resize(function() {
+	
+		var widthS = $(window).width();
+		if (widthS < 851) {
+			$('#menu-btn').addClass('closed');
+			$('#left_nav').css({width:'0px',left:'-300px',visibility:'hidden'});		
+		}
+		else {
+			$('#menu-btn').removeClass('closed');
+			$('#left_nav').css({width:'252px',left:'0px',visibility:'visible'});				
+		}
+	});
+});	
+
+$(document).ready(function(){
+		
+		$('#menu-btn').click(function(){
+				if ($("#menu-btn").hasClass("closed")) {
+		    		 $('#left_nav').animate({width:'252px',left:'0px'}, 'slow'); 	 
+		    	   	 $('#left_nav').css('visibility','visible');			
+		    		 $('#menu-btn').removeClass("closed");
+				}
+				else {
+			    	 	 $('#left_nav').animate({width:'0px',left:'-300px'}, 'slow'); 	 
+			    	   	 $('#left_nav').css('visibility','none');
+			    	   	 $('#menu-btn').addClass("closed");
+			     }
+    	});
+    	     
+});
+
+
+
+/*----------------------- atricles type menu ----------------------- */
+
+$(document).ready(function() {
+    
+    $('ul li a').each(function() {
+       	 $(this).css('width', $(this).width() );
+    });
+});
+
+/*Background scrolling pictures start*/
+
+/*
+var BackgroundScroll = function(params) {
+	params = $.extend({
+		scrollSpeed: 20,
+		imageWidth: $('#textual_pages').width(),
+		imageHeight: $('#textual_pages').height(),
+		imageWidthHp: $('#hp').width(),
+		imageHeightHp: $('#hp').height()
+	}, params);
+	
+	var step = 1,
+		current = 0,
+		restartPosition = - (params.imageWidth - params.imageHeight);
+	
+	var scroll = function() {
+		current -= step;
+		if (current == restartPosition){
+			current = 0;
+		}	
+		$('#textual_pages').css('backgroundPosition', current + 'px 0');
+		$('#hp').css('backgroundPosition', current + 'px 0');
+	
+	};
+	
+	this.init = function() {
+		setInterval(scroll, params.scrollSpeed);
+	
+	};
+};
+
+var scroll = new BackgroundScroll();
+scroll.init();*/
+
+
+
+
+
